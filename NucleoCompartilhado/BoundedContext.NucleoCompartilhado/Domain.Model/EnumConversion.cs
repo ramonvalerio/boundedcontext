634753e4e7b6a600cc81d6f9060fb7d4def7b90c
Ramon Valerio <ramonvalerios@gmail.com>
2018-03-12 18:07:36 -0300

    Implementação da classe EnumConversion

---
+++ NucleoCompartilhado/BoundedContext.NucleoCompartilhado/Domain.Model/EnumConversion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BoundedContext.NucleoCompartilhado.Domain.Model
+{
+    public static class EnumConversion
+    {
+        public static T ToEnum<T>(this string value) => (T)Enum.Parse(typeof(T), value);
+        public static T ToEnum<T>(this int value) => (T)Enum.ToObject(typeof(T), value);
+    }
+}
