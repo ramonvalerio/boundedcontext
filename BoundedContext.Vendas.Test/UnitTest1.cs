99dcbb01f17c373cca557433888b7d1945de4abe
Ramon Valerio <ramonvalerios@gmail.com>
2018-03-16 02:20:47 -0300

    Camada de Testes para o BoundedContext de Vendas

---
+++ BoundedContext.Vendas.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace BoundedContext.Vendas.Test
+{
+    /// <summary>
+    /// RED, GREEN, REFACTOR
+    /// </summary>
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
